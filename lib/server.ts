41e2d287ba7ed6d3b5e5996b4f7bf8a53dbea58a
Beeno Tung <aabbcc1241@yahoo.com.hk>
2024-09-22 09:51:22 +0800

    patch: fix typo on require()

--- lib/server.ts
+++ lib/server.ts
@@ -1,7 +1,7 @@
 import type { Application, Router } from 'express'
 
 export function attachFrameRoute(app: Router | Application) {
-  let { resolve, join } = eval('request("path")')
+  let { resolve, join } = eval('require("path")')
   let frame_file = resolve(join(__dirname, 'frame.html'))
   let router = app as Router
   router.get('/frame.html', (req, res) => {

a0d786e3f996ad07a2ad6d6068c7672f43074e33
Beeno Tung <aabbcc1241@yahoo.com.hk>
2024-09-22 09:22:08 +0800

    patch: fix for esbuild bundle

--- lib/server.ts
+++ lib/server.ts
@@ -1,7 +1,7 @@
 import type { Application, Router } from 'express'
 
 export async function attachFrameRoute(app: Router | Application) {
-  let { resolve, join } = await import('path')
+  let { resolve, join } = await eval('import("path")')
   let frame_file = resolve(join(__dirname, 'frame.html'))
   let router = app as Router
   router.get('/frame.html', (req, res) => {
